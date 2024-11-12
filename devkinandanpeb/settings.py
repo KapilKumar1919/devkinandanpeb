84de953c746258132d60d23acc9f4cbfd895f514
Kapil <gauravkulshrestha1919@gmail.com>
2024-11-12 21:58:50 +0530

    allowed-hosts-updated

--- devkinandanpeb/settings.py
+++ devkinandanpeb/settings.py
@@ -15,8 +15,7 @@ SECRET_KEY = 'django-insecure-89qx)pmxvs@4k&gch)jza4#i=##w@o&+=7e-3p)(u5n-1!zop*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['13.201.104.248', 'www.devkinandanpeb.com', 'devkinandanpeb.com']
 
 # Application definition
 
