0e2c92dbbe4de3fafb143eba3216fc2d3bcf2765
Kapil <gauravkulshrestha1919@gmail.com>
2024-11-14 13:55:04 +0530

    updated-docker-file

--- devkinandanpeb/settings.py
+++ devkinandanpeb/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,7 +14,7 @@ SECRET_KEY = 'django-insecure-89qx)pmxvs@4k&gch)jza4#i=##w@o&+=7e-3p)(u5n-1!zop*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.201.104.248', 'www.devkinandanpeb.com', 'devkinandanpeb.com']
+ALLOWED_HOSTS = ['13.201.104.248', 'www.devkinandanpeb.com', 'devkinandanpeb.com', '127.0.0.1']
 
 # Application definition
 
