9f447db61737a38fbf2e66955b6a02bcd200ce0c
Kapil <gauravkulshrestha1919@gmail.com>
2024-11-14 13:31:46 +0530

    updated-docker-file

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /app
 COPY . /app
 
 # Install any needed packages specified in requirements.txt
-RUN pip install -r requirements.txt
+RUN pip install -r requirements.txt --no-cache-dir
 
 # Copy the fixtures directory to the container
 COPY fixtures /app/fixtures
--- devkinandanpeb/settings.py
+++ devkinandanpeb/settings.py
@@ -68,23 +68,23 @@ WSGI_APPLICATION = 'devkinandanpeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'devkinandanpeb',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'devkinandanpeb.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'devkinandanpeb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
