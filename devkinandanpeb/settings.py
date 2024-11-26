31937d1fdaf4cc5c1cb91841414d5c7fd73b4b30
Kapil <gauravkulshrestha1919@gmail.com>
2024-11-26 21:14:44 +0530

    updated

--- devkinandanpeb/settings.py
+++ devkinandanpeb/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = ['devkinandanpeb.com', 'www.devkinandanpeb.com', '3.110.55.255',
 # Application definition
 
 INSTALLED_APPS = [
-    # 'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +38,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
